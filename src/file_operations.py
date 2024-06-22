e7110124f4732a3736f27a8f27d1ee1051266ec0
jayel888 <15644@coderacademy.edu.au>
2024-06-22 12:36:07 +1000

    match operations added

--- src/file_operations.py
+++ src/file_operations.py
@@ -1,22 +1,22 @@
 import json
 
 def load_matches(file_path):
+    """
+    Load matches from a JSON File.
 
-    # Load matches from a JSON File.
-
-    # parameters: file_path: Path to the JSON file
-    # return: List of matches or an empty list if an error occurs
-
+    parameters: file_path: Path to the JSON file
+    return: List of matches or an empty list if an error occurs
+    """
     try:
         with open(file_path, 'r') as file:
             matches = json.load(file)
         return matches
     except FileNotFoundError:
         print(f"Error: The file {file_path} does not exist.")
-        return[]
+        return []
     except Exception as e:
-        print(f"An unexpected error has occured: {e}")
-        return[]
+        print(f"An unexpected error occured: {e}")
+        return []
     
 def save_matches(file_path, matches):
     try:
